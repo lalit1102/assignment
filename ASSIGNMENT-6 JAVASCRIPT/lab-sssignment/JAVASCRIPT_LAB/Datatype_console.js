d3796de25841096381373c9d38890bc5ba25e6b2
Lalit Baldaniya <lalit.baldaniya92@gmail.com>
2025-10-03 18:17:15 +0530

    javascript
    javascript

---
+++ ASSIGNMENT-6 JAVASCRIPT/lab-sssignment/JAVASCRIPT_LAB/Datatype_console.js
@@ -0,0 +1,32 @@
+// String
+let myString = "Hello, JavaScript!";
+console.log("Value:", myString, "| Type:", typeof myString);
+
+// Number
+let myNumber = 42;
+console.log("Value:", myNumber, "| Type:", typeof myNumber);
+
+// Boolean
+let myBoolean = true;
+console.log("Value:", myBoolean, "| Type:", typeof myBoolean);
+
+// Null
+let myNull = null;
+console.log("Value:", myNull, "| Type:", typeof myNull);  // Special case: type is "object"
+
+// Undefined
+let myUndefined;
+console.log("Value:", myUndefined, "| Type:", typeof myUndefined);
+
+// Bigint
+let bigNumber = 1234567890123456789012345678901234567890n;
+console.log("BigInt 1:", bigNumber);
+
+// symbol
+const sym1 = Symbol();
+const sym2 = Symbol("foo");
+const sym3 = Symbol("foo");
+console.log(`${sym1} and ${sym2} and ${sym3}`)
+
+
+
---
+++ ASSIGNMENT-6 JAVASCRIPT/lab-sssignment/JAVASCRIPT_LAB/variable_dec.js
@@ -0,0 +1,23 @@
+// String
+let myString = "Hello, JavaScript!";
+console.log("Value:", myString, "| Type:", typeof myString);
+
+// Number
+let myNumber = 42;
+console.log("Value:", myNumber, "| Type:", typeof myNumber);
+
+// Boolean
+let myBoolean = true;
+console.log("Value:", myBoolean, "| Type:", typeof myBoolean);
+
+// Null
+let myNull = null;
+console.log("Value:", myNull, "| Type:", typeof myNull);  // Special case: type is "object"
+
+// Undefined
+let myUndefined;
+console.log("Value:", myUndefined, "| Type:", typeof myUndefined);
+
+// Bigint
+let bigNumber = 1234567890123456789012345678901234567890n;
+console.log("BigInt 1:", bigNumber);
